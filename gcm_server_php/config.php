e1d02c5791d5dcaa82052107000a4f3409a3f220
Valentine Aikhu <valentineaikhu@gmail.com>
2014-06-18 17:32:33 -0700

    config and db_connect updated

---
+++ gcm_server_php/config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Database config variables
+ */
+define("DB_HOST", "localhost");
+define("DB_USER", "easyDeveloper");
+define("DB_PASSWORD", "X,L8(Be=aM3T");
+define("DB_DATABASE", "gcm");
+
+/*
+ * Google API Key
+ */
+define("GOOGLE_API_KEY", "AIzaSyAeB_1xt_hDvLs86Hz9ZZF-hNPwxuxHPew"); // Place your Google API Key
+?>
